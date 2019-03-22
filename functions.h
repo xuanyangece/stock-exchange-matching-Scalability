d534fb58246379c9e1536cad6e892af33dd4649d
Xuan Yang <xy91@duke.edu>
2019-03-22 16:53:25 -0400

    Prototype

--- functions.h
+++ functions.h
@@ -1,3 +1,17 @@
 #include <iostream>
 
-void handleXML(int reqfd);
+void handleXML(int reqfd); // parse
+
+void create(int reqfd); // create account or symbol
+
+void createAccount(int reqfd);
+
+void createSymbol(int reqfd);
+
+void transactions(int reqfd); // deal with transactions
+
+void order(int reqfd);
+
+void cancel(int reqfd);
+
+void query(int reqfd);
