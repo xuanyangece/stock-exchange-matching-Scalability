cb1aa4fb85fed0058280119826b5e64aacbbcbe6
Kai Zhang <kai.zhang01@duke.edu>
2019-03-24 20:15:21 -0400

    fix merge error

--- functions.cpp
+++ functions.cpp
@@ -2,82 +2,89 @@
 
 #define DEBUG 1
 #define BUFFSIZE 40960
+
+// All for fixed string
 #define ID "id"
 #define BALANCE "balance"
 #define SYM "sym"
 #define NUM "num"
+#define AMOUNT "amount"
+#define LIMIT "limit"
 
 /*
     Receive XML from client and keep connection until invalid format.
 */
 void handleXML(int client_fd) {
-  // loop and dispatch
-  char buffer[BUFFSIZE];
+    // loop and dispatch
+    char buffer[BUFFSIZE];
 
-  while (1) {
-    // Reset buffer
-    memset(buffer, '\0', sizeof(buffer));
+    while (1) {
+        // Reset buffer
+        memset(buffer, '\0', sizeof(buffer));
 
-    // Recv
-    int totalsize = recv(client_fd, buffer, BUFFSIZE, 0);
-    if (totalsize < 0)
-      std::cout << "Error receive buffer from client." << std::endl;
+        // Recv
+        int totalsize = recv(client_fd, buffer, BUFFSIZE, 0);
+        if (totalsize < 0) std::cout<<"Error receive buffer from client."<<std::endl;
 
-    std::string xml(buffer);
+        std::string xml(buffer);
 
-    if (DEBUG)
-      std::cout << "Buffer received from client: " << std::endl << xml << std::endl;
+        if (DEBUG) std::cout<<std::endl<<"Buffer received from client: "<<std::endl<<xml<<std::endl<<std::endl;
 
-    // Assume it's correct
-    if (xml.find("<create>") != std::string::npos) {
-      create(xml);
+        // Assume it's correct
+        if (xml.find("<create>") != std::string::npos) {
+            create(xml);
+        }
+        else if (xml.find("<transactions>") != std::string::npos) {
+            transactions(xml);
+        }
+        else {
+            // Close connection
+            close(client_fd);
+            break;
+        }
     }
-    else if (xml.find("<transactions>") != std::string::npos) {
-      transactions(xml);
-    }
-    else {
-      // Close connection
-      close(client_fd);
-      break;
-    }
-  }
 }
 
 /*
-    Create symbol or account.
+    Parse create and dispatch different request.
 */
 void create(std::string xml) {
-  // Parse by line, greedy
-  while (1) {
-    // Get line by line break's position
-    size_t linebreak = xml.find('\n');
+    // Parse by line, greedy
+    while (1) {
+        // Get line by line break's position, xml stands for the remaining content to be parsed
+        size_t linebreak = xml.find('\n');
+        if (linebreak == std::string::npos) break;
 
-    if (linebreak == std::string::npos)
-      break;
+        std::string line = xml.substr(0, linebreak);
 
-    std::string line = xml.substr(0, linebreak);
+        if (line.find("account") != std::string::npos) {
+            // Create account: parse one line is enough
+            std::string id = getAttribute(line, ID);
+            std::string balance = getAttribute(line, BALANCE);
+            createAccount(id, balance);
 
-    if (line.find("account") != std::string::npos) {
-      // Create account: pass line is enough
-      std::string id = getAttribute(line, ID);
-      std::string balance = getAttribute(line, BALANCE);
-      createAccount(id, balance);
+            // Skip one line
+            xml = xml.substr(linebreak + 1);
+        }
+        else if (line.find("symbol") != std::string::npos) {
+            // Create symbol: need to parse the whole symbol tag
+            std::string symbol = getAttribute(line, SYM);
+            
+            // Get the info inside symbol tag
+            size_t start = xml.find('\n') + 1;      // skip the <symbol ...> line
+            size_t end = xml.find("</symbol>");     // end before </symbol>, containing line break!
+            std::string accounts = xml.substr(start, end - start);
 
-      // Skip one line
-      xml = xml.substr(linebreak + 1);
+            parseSymbol(accounts, symbol);
+
+            // Skip the entire symbol tag
+            xml = xml.substr(end + 10);
+        }
+        else {
+            // Otherwise skip one line
+            xml = xml.substr(linebreak + 1);
+        }
     }
-    else if (line.find("symbol") != std::string::npos) {
-      // Create symbol: need to pass remain xml because of multiple lines
-      std::string id = getAttribute(xml, ID);
-      std::string symbol = getAttribute(xml, SYM);
-      std::string amount = getAttribute(xml, NUM);
-      createSymbol(id, symbol, amount);
-
-      // Skil multiple lines
-      size_t end_of_sym = xml.find("</symbol>");
-      xml = xml.substr(end_of_sym + 10);  // skip the word "</symbol>"
-    }
-  }
 }
 
 /*
@@ -85,24 +92,95 @@ void create(std::string xml) {
     In our deisn, it accesses the first one.
 */
 std::string getAttribute(std::string remain, std::string attribute) {
-  std::string ans;
+    std::string ans;
+    
+    if (attribute == NUM) {
+        // NUM is between first '>' and second '<'
+        size_t pos = remain.find('>') + 1;
+        while (remain[pos] != '<') {
+            ans += remain[pos++];
+        }
+    }
+    else {
+        size_t pos = remain.find(attribute);
+        
+        // try to find open parenthesis
+        while (remain[pos] != '\"') {
+            pos++;
+        }
 
-  if (attribute == NUM) {
-  }
-  else {
-    size_t pos = remain.find(attribute);
-
-    // try to find open parenthesis
-    while (remain[pos] != '\"') {
-      pos++;
+        pos++; // skip open parenthesis
+        
+        while (remain[pos] != '\"') {
+            ans += remain[pos++];
+        }
     }
 
-    pos++;  // skip open parenthesis
+    return ans;
+}
 
-    while (remain[pos] != '\"') {
-      ans += remain[pos++];
+/*
+    Parse the entire symbol body:
+    Do greedy to find all acounts and number of symbols to be added.
+*/
+void parseSymbol(std::string accounts, std::string symbol) {
+    // Basically access each line and call createSymbol
+    while (true) {
+        size_t linebreak = accounts.find('\n');
+        if (linebreak == std::string::npos) break; // end of parsing
+
+        std::string id = getAttribute(accounts, ID);
+        std::string amount = getAttribute(accounts, NUM);
+        createSymbol(id, symbol, amount);
+
+        // skip current acount
+        accounts = accounts.substr(linebreak + 1);
     }
-  }
+}
 
-  return ans;
+/*
+    Parse transactions and dispatch different request.
+*/
+void transactions(std::string xml) {
+    // Access account ID at first and get all requests
+    std::string account_id = getAttribute(xml, ID);
+
+    size_t start = xml.find('\n') + 1;  // // skip the <transactions ...> line
+    size_t end = xml.find("</transactions>");
+    std::string requests = xml.substr(start, end - start);
+
+    if (DEBUG) std::cout<<std::endl<<"All requests from transactions: "<<std::endl<<requests<<std::endl<<std::endl;
+
+    // Parse line by line: greedy
+    while (1) {
+        // Get line by linebreak's position, xml stands for the remaining content to be parsed
+        size_t linebreak = requests.find('\n');
+        if (linebreak == std::string::npos) break;
+
+        std::string line = xml.substr(0, linebreak);
+
+        if (line.find("order") != std::string::npos) {
+            // Access symbol, amount and limit
+            std::string symbol = getAttribute(line, SYM);
+            std::string amount = getAttribute(line, AMOUNT);
+            std::string limit = getAttribute(line, LIMIT);
+
+            order(account_id, symbol, amount, limit);
+        }
+        else if (line.find("query") != std::string::npos) {
+            // Access trans_id
+            std::string trans_id = getAttribute(line, ID);
+
+            query(account_id, trans_id);
+        }
+        else if (line.find("cancel") != std::string::npos) {
+            // Access trans_id
+            std::string trans_id = getAttribute(line, ID);
+
+            query(account_id, trans_id);
+        }
+
+        // Skip one line
+        requests = requests.substr(linebreak + 1);
+    }
 }
--- functions.h
+++ functions.h
@@ -1,29 +1,29 @@
+#include <iostream>
+#include <exception>
+#include <mutex>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <sys/select.h>
-#include <unistd.h>
-
-#include <cstring>
-#include <exception>
-#include <iostream>
-#include <mutex>
-#include <string>
 #include <thread>
+#include <unistd.h>
+#include <string>
 
-void handleXML(int client_fd);  // parse
+void handleXML(int client_fd); // parse
 
-void create(std::string xml);  // create account or symbol
+void create(std::string xml); // create account or symbol
 
 void createAccount(std::string id, std::string balance);
 
+void parseSymbol(std::string accounts, std::string symbol);
+
 void createSymbol(std::string id, std::string symbol, std::string amount);
 
-void transactions(std::string xml);  // deal with transactions
+void transactions(std::string xml); // deal with transactions
 
-void order(int client_fd);
+void order(std::string account_id, std::string symbol, std::string amount, std::string limit);
 
-void cancel(int client_fd);
+void cancel(std::string account_id, std::string trans_id);
 
-void query(int client_fd);
+void query(std::string account_id, std::string trans_id);
 
 std::string getAttribute(std::string remain, std::string attribute);
