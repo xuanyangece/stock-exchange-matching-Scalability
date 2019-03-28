8ed87565d7bb0017382819d3d8ab753e2ee3fa05
Kai Zhang <kai.zhang01@duke.edu>
2019-03-27 23:04:25 -0400

    Add serial test cases and finish serial testing

---
+++ testSerial/r.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+./client r1.txt
+./client r2.txt
+./client r3.txt
+./client r4.txt
+./client r5.txt
+./client r6.txt
+./client r7.txt
+./client r8.txt
+./client r9.txt
+./client r10.txt
+./client r11.txt
---
+++ testSerial/t.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+./client t0.txt
+./client t1.txt
+./client t2.txt
+./client t3.txt
+./client t4.txt
+./client t5.txt
+./client t6.txt
+./client t7.txt
+./client t8.txt
